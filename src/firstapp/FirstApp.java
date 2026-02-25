b8124fbce359411b61756164feb3b14f7cd38f90
ST10513480 <st10513480@myemeris.edu.za>
2026-02-25 09:04:52 +0200

    My First Commit

---
+++ src/firstapp/FirstApp.java
@@ -0,0 +1,34 @@
+//CODE THAT WILL PRINT A SIGN BY USING ESCAPE SEQUENCES AND USING COMMENTS
+/**
+ * THERE IS 3 TYPES OF COMMENTSS:
+ * A LINE COMMENT ('//')
+ * A BLOCK COMMENT ('/*')
+ * A DOCUMENT COMMENT ('/**')
+ */
+package firstapp;
+
+/**
+ *
+ * @author gabrielkatzman
+ * @version 1.1
+ */
+public class FirstApp {
+
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+
+System.out.println("Hello, Welcome to the world of programming!!!!");
+
+//Displaying the string using an escape sequence which in this case is '\n & \t'
+System.out.println("Hello, \nWelcome\nto\nthe\nworld\nof\nprogramming!!!!");
+
+System.out.println("\t     / \\ \n\t    /   \\\n\t   /     \\\n\t  /       \\\n\t /_ _ _ _ _\\");
+System.out.println("\t     | |\n\t     | |\n\t     | |\n\t     | |");
+System.out.println("\t[@@@@@@@@@@@]");
+
+System.out.println("\n\n\t  | | | | | | | |  \n\t |\t\t |\n\t|\t\t  |\n \t|\t\t  |");
+
+    }
+}
